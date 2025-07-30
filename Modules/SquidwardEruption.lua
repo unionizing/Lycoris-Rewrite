eaf3849e57ea1732b1606f8426f9a388b1ac774e
John Doe <johndoe@example.com>
2025-07-30 08:20:51 -0700

    feat: Update action type to "Dodge" in SquidwardEruption and adjust timing logic in TitusVault

--- Modules/SquidwardEruption.lua
+++ Modules/SquidwardEruption.lua
@@ -8,7 +8,7 @@ return function(self, timing)
 	local distance = self:distance(self.entity)
 	local action = Action.new()
 	action._when = 400 + (distance * 4)
-	action._type = "Parry"
+	action._type = "Dodge"
 	action.hitbox = Vector3.new(40, 40, 40)
 	action.name = string.format("(%.2f) Dynamic Squidward Timing", distance)
 	return self:action(timing, action)
