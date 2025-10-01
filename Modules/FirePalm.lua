9e3be42cd13cd05a7589e43ebc95b06f1f10bb38
juanitoelgordito <221791452+juanitoelgordito@users.noreply.github.com>
2025-10-01 13:10:50 -0700

    Refactor animation and sound assets for improved gameplay mechanics
    - Modified hitbox dimensions and timings for various animation actions to enhance responsiveness and player experience.
    - Updated the "Parry" action across multiple assets, adjusting hitbox sizes and timings to ensure better alignment with gameplay.
    - Added new sound assets and removed outdated ones to reflect changes in animation and gameplay mechanics.
    - Adjusted the "imxd" parameter for several animations to fine-tune their impact and effectiveness during gameplay.
    - Consolidated changes across multiple patches to streamline asset management and improve overall performance.

---
+++ Modules/FirePalm.lua
@@ -0,0 +1,28 @@
+---@class Action
+local Action = getfenv().Action
+
+---Module function.
+---@param self AnimatorDefender
+---@param timing AnimationTiming
+return function(self, timing)
+	local hrp = self.entity:FindFirstChild("HumanoidRootPart")
+	if not hrp then
+		return
+	end
+
+	if hrp:WaitForChild("REP_SOUND_4377231054", 0.1) then
+		local action = Action.new()
+		action._when = 450
+		action._type = "Parry"
+		action.hitbox = Vector3.new(30, 20, 30)
+		action.name = "Gale Punch Timing"
+		return self:action(timing, action)
+	else
+		local action = Action.new()
+		action._when = 540
+		action._type = "Parry"
+		action.hitbox = Vector3.new(28, 20, 43)
+		action.name = "Fire Palm Timing"
+		return self:action(timing, action)
+	end
+end
