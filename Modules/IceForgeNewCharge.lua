6c543f1701f7da88a21d071431960961e6ba8f44
juanitoelgordito <221791452+juanitoelgordito@users.noreply.github.com>
2025-11-09 16:33:29 -0800

    fix(IceForgeNewCharge): Resolve merge conflicts and clean up action definitions feat(Timings): Add new patch files with updated animation parameters and hitbox adjustments

--- Modules/IceForgeNewCharge.lua
+++ Modules/IceForgeNewCharge.lua
@@ -15,28 +15,15 @@ local Defense = getfenv().Defense
 ---@param self AnimatorDefender
 ---@param timing AnimationTiming
 return function(self, timing)
-<<<<<<< HEAD
-=======
-	local thrown = workspace:FindFirstChild("Thrown")
-	if not thrown then
-		return
-	end
-
->>>>>>> 863c67d697eacb89954261894622f74a02f7a343
 	local tracker = ProjectileTracker.new(function(candidate)
 		return candidate.Name == "IceShuriken"
 	end)
 
 	task.wait(0.20 - self.rtt())
-<<<<<<< HEAD
-=======
-
->>>>>>> 863c67d697eacb89954261894622f74a02f7a343
 	if self:distance(self.entity) <= 20 then
 		local action = Action.new()
 		action._type = "Start Block"
 		action._when = 0
-<<<<<<< HEAD
 		action.hitbox = Vector3.new(50, 50, 50)
 		action.name = "Ice Forge Close Timing"
 		self:action(timing, action)
@@ -46,20 +33,6 @@ return function(self, timing)
 		actionTwo._when = 800
 		actionTwo.hitbox = Vector3.new(80, 80, 80)
 		self:action(timing, actionTwo)
-=======
-		action.name = "Ice Forge Close Timing"
-		action.fhb = true
-		action.hitbox = Vector3.new(35, 35, 35)
-		self:action(timing, action)
-
-		local actionTwo = Action.new()
-		actionTwo.name = "Ice Forge Close End Timing"
-		actionTwo._type = "End Block"
-		actionTwo._when = 1400
-		actionTwo.fhb = true
-		actionTwo.ihbc = true
-		return self:action(timing, actionTwo)
->>>>>>> 863c67d697eacb89954261894622f74a02f7a343
 	end
 
 	local action = Action.new()
